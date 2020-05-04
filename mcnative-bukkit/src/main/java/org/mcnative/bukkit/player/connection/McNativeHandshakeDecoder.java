af4eb0581561f946e0fe2e7b589323193d09875d
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2020-05-04 20:27:44 +0200

    Disabled McNativeHandshakeDecoder temporarily

--- mcnative-bukkit/src/main/java/org/mcnative/bukkit/player/connection/McNativeHandshakeDecoder.java
+++ mcnative-bukkit/src/main/java/org/mcnative/bukkit/player/connection/McNativeHandshakeDecoder.java
@@ -21,7 +21,6 @@ package org.mcnative.bukkit.player.connection;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import org.bukkit.event.server.ServerListPingEvent;
@@ -35,7 +34,6 @@ import org.mcnative.common.protocol.MinecraftProtocolVersion;
 
 import java.net.InetSocketAddress;
 import java.util.List;
-import java.util.Map;
 
 //@Todo implement legacy < 1.6 status ping
 public class McNativeHandshakeDecoder extends MessageToMessageDecoder<ByteBuf> {
@@ -61,17 +59,14 @@ public class McNativeHandshakeDecoder extends MessageToMessageDecoder<ByteBuf> {
         if(!finished){
             finished = true;
             int packetId = MinecraftProtocolUtil.readVarInt(buffer);
-            if(packetId == 10){
-                for (Map.Entry<String, ChannelHandler> entry : connection.getChannel().pipeline()) {
-                    System.out.println(entry.getKey()+" | "+entry.getValue());
-                }
+            if(packetId == HANDSHAKE_PACKET_ID){
 
                 int protocolVersion = MinecraftProtocolUtil.readVarInt(buffer);
                 String host = MinecraftProtocolUtil.readString(buffer);
                 int port = buffer.readUnsignedShort();
                 int next = MinecraftProtocolUtil.readVarInt(buffer);
                 connection.initHandshake(protocolVersion,host,port);
-                if(next == 1){
+                if(next == 10){
                     statusRequest = true;
                     handleServerListPing();
                     out.release();
