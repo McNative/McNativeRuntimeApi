bc7a6a29fc8f2228e72a204bba6f88aa14c76bbe
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2020-12-29 22:15:57 +0100

    Implemented ServerStatusRequester

---
+++ mcnative-runtime-api/src/main/java/org/mcnative/runtime/api/network/component/server/ServerStatusRequester.java
@@ -0,0 +1,41 @@
+/*
+ * (C) Copyright 2020 The McNative Project (Davide Wietlisbach & Philipp Elvin Friedhoff)
+ *
+ * @author Davide Wietlisbach
+ * @since 18.07.20, 15:35
+ * @web %web%
+ *
+ * The McNative Project is under the Apache License, version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.mcnative.runtime.api.network.component.server;
+
+import org.mcnative.runtime.api.McNative;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
+
+public interface ServerStatusRequester {
+
+    CompletableFuture<ServerStatusResponse> requestStatusAsync(MinecraftServer server);
+
+    CompletableFuture<ServerStatusResponse> requestStatusAsync(InetSocketAddress address);
+
+    ServerStatusResponse requestStatus(MinecraftServer server);
+
+    ServerStatusResponse requestStatus(InetSocketAddress address);
+
+    static ServerStatusRequester getDefault() {
+        return McNative.getInstance().getRegistry().getService(ServerStatusRequester.class);
+    }
+}
