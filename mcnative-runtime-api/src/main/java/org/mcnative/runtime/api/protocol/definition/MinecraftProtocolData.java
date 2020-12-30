edf0ca7aa5d15bfa665de5a3929cf354a24775a5
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2020-12-30 14:13:33 +0100

    Renamed MinecraftProtocolEntry to MinecraftProtocolData

--- mcnative-runtime-api/src/main/java/org/mcnative/runtime/api/protocol/definition/MinecraftProtocolData.java
+++ mcnative-runtime-api/src/main/java/org/mcnative/runtime/api/protocol/definition/MinecraftProtocolData.java
@@ -3,13 +3,13 @@ package org.mcnative.runtime.api.protocol.definition;
 import org.mcnative.runtime.api.protocol.packet.MinecraftPacket;
 import org.mcnative.runtime.api.protocol.packet.MinecraftPacketCodec;
 
-public class MinecraftProtocolEntry {
+public class MinecraftProtocolData {
 
     private final int packetId;
     private final Class<? extends MinecraftPacket> packetClass;
     private final MinecraftPacketCodec<?> codec;
 
-    public MinecraftProtocolEntry(int packetId, Class<? extends MinecraftPacket> packetClass, MinecraftPacketCodec<?> codec) {
+    public MinecraftProtocolData(int packetId, Class<? extends MinecraftPacket> packetClass, MinecraftPacketCodec<?> codec) {
         this.packetId = packetId;
         this.packetClass = packetClass;
         this.codec = codec;
