d3dc0cba6d7d1b9a35a2392404f8d0beb5aefc95
Fridious <philippfriedhoff@outlook.de>
2021-03-27 16:55:41 +0100

    Implemented more item data

---
+++ mcnative-runtime-api-service/src/main/java/org/mcnative/runtime/api/service/inventory/item/data/banner/BannerItemData.java
@@ -0,0 +1,8 @@
+package org.mcnative.runtime.api.service.inventory.item.data.banner;
+
+import org.mcnative.runtime.api.service.inventory.item.data.ItemData;
+
+public interface BannerItemData extends ItemData {
+
+
+}
---
+++ mcnative-runtime-api-service/src/main/java/org/mcnative/runtime/api/service/inventory/item/data/banner/BannerPattern.java
@@ -0,0 +1,5 @@
+package org.mcnative.runtime.api.service.inventory.item.data.banner;
+
+public class BannerPattern {
+
+}
