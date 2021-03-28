d62bca51b81a7883487c0df7eab61fea6a6658dc
Fridious <philippfriedhoff@outlook.de>
2021-03-28 18:45:56 +0200

    Added LeatherArmorItemData

---
+++ mcnative-runtime-api-service/src/main/java/org/mcnative/runtime/api/service/inventory/item/data/LeatherArmorItemData.java
@@ -0,0 +1,10 @@
+package org.mcnative.runtime.api.service.inventory.item.data;
+
+import java.awt.*;
+
+public interface LeatherArmorItemData extends ItemData {
+
+    Color getColor();
+
+    LeatherArmorItemData setColor(Color color);
+}
