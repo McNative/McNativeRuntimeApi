2f57997b1c41a2c6d448af914beecd9aeef7ee2b
Fridious <philippfriedhoff@outlook.de>
2021-03-28 14:47:48 +0200

    Added BannerItemData api

--- mcnative-runtime-api-service/src/main/java/org/mcnative/runtime/api/service/inventory/item/data/banner/BannerItemData.java
+++ mcnative-runtime-api-service/src/main/java/org/mcnative/runtime/api/service/inventory/item/data/banner/BannerItemData.java
@@ -1,8 +1,26 @@
 package org.mcnative.runtime.api.service.inventory.item.data.banner;
 
+import org.mcnative.runtime.api.service.inventory.item.DyeColor;
 import org.mcnative.runtime.api.service.inventory.item.data.ItemData;
+
+import java.util.List;
 
 public interface BannerItemData extends ItemData {
 
+    DyeColor getBaseColor();
 
+    BannerItemData setBaseColor(DyeColor color);
+
+
+    List<BannerPattern> getPatterns();
+
+    BannerItemData setPatterns(List<BannerPattern> patterns);
+
+    void setPattern(int index, BannerPattern pattern);
+
+    BannerItemData addPattern(BannerPattern pattern);
+
+    BannerItemData removePattern(BannerPattern pattern);
+
+    BannerItemData removePattern(int index);
 }
