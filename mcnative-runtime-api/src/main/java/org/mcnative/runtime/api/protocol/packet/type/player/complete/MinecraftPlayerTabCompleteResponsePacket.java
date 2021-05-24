4f7317327a1e3356329a68dc50993bcac1ab6827
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-05-24 12:52:12 +0200

    Implemented new player tab complete packets

---
+++ mcnative-runtime-api/src/main/java/org/mcnative/runtime/api/protocol/packet/type/player/complete/MinecraftPlayerTabCompleteResponsePacket.java
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 2020 The McNative Project (Davide Wietlisbach & Philipp Elvin Friedhoff)
+ *
+ * @author Davide Wietlisbach
+ * @since 18.04.20, 20:20
+ * @web %web%
+ *
+ * The McNative Project is under the Apache License, version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.mcnative.runtime.api.protocol.packet.type.player.complete;
+
+import org.mcnative.runtime.api.protocol.packet.MinecraftPacket;
+
+import java.util.Collection;
+
+public class MinecraftPlayerTabCompleteResponsePacket implements MinecraftPacket {
+
+    private int transactionId;
+    private Collection<String> suggestions;
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Collection<String> getSuggestions() {
+        return suggestions;
+    }
+
+    public void setSuggestions(Collection<String> suggestions) {
+        this.suggestions = suggestions;
+    }
+}
