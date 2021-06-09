d50112a65e369e6af076db3bc39eb88734de5e03
Fridious <philippfriedhoff@outlook.de>
2021-06-09 02:32:02 +0200

    Fixed StringLengthPlayerTextInputValidator default constructor

--- mcnative-runtime-api/src/main/java/org/mcnative/runtime/api/player/input/types/StringLengthPlayerTextInputValidator.java
+++ mcnative-runtime-api/src/main/java/org/mcnative/runtime/api/player/input/types/StringLengthPlayerTextInputValidator.java
@@ -6,6 +6,10 @@ import org.mcnative.runtime.api.text.components.MessageComponent;
 public class StringLengthPlayerTextInputValidator extends TextPlayerTextInputValidator {
 
     public StringLengthPlayerTextInputValidator(int maxLength, MessageComponent<?> errorMessage) {
-        super(input -> input.length() <= maxLength, Text.ofMessageKey("mcnative.textinput.validator.error.stringLength"));
+        super(input -> input.length() <= maxLength, errorMessage);
+    }
+
+    public StringLengthPlayerTextInputValidator(int maxLength) {
+        this(maxLength, Text.ofMessageKey("mcnative.textinput.validator.error.stringLength"));
     }
 }
