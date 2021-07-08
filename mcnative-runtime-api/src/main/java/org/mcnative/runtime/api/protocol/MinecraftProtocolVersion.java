f0ccf9393a5a2c1512e57f71cd5322cdc9eb2e5c
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-07-08 19:26:00 +0200

    Added 1.17.1 protocol version

--- mcnative-runtime-api/src/main/java/org/mcnative/runtime/api/protocol/MinecraftProtocolVersion.java
+++ mcnative-runtime-api/src/main/java/org/mcnative/runtime/api/protocol/MinecraftProtocolVersion.java
@@ -29,6 +29,7 @@ public enum MinecraftProtocolVersion {
 
     /** Java Edition */
 
+    JE_1_17_1(756,"1.17.1",MinecraftEdition.JAVA),
     JE_1_17(755,"1.17",MinecraftEdition.JAVA),
     JE_1_16_4(754,"1.16.4",MinecraftEdition.JAVA),
     JE_1_16_3(753,"1.16.3",MinecraftEdition.JAVA),
